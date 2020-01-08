0317bc8b17d9b2047517159ce1b30ef16e295ccb
Pedro Bini <dev.pedrobini@gmail.com>
2020-01-08 11:21:12 -0300

    Import react-native-music-control on App.js

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MusicControl from 'react-native-music-control';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Open up App.js to start working on your app!
+      </Text>
+      <Text>
+        {MusicControl.RATING_4_STARS}
+      </Text>
     </View>
   );
 }
